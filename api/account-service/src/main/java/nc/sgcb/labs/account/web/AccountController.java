3375924def891a9df44a500696c1261b82b7bb06
Jerome WACONGNE <j.wacongne@tahitinumerique.pf>
2026-07-10 17:16:54 -1000

    Proper Pageable OpenAPI doc

--- api/account-service/src/main/java/nc/sgcb/labs/account/web/AccountController.java
+++ api/account-service/src/main/java/nc/sgcb/labs/account/web/AccountController.java
@@ -1,5 +1,21 @@
 package nc.sgcb.labs.account.web;
 
+import java.util.List;
+import java.util.Objects;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,16 +27,6 @@ import nc.sgcb.labs.account.jpa.AccountJpaRepository;
 import nc.sgcb.labs.account.jpa.MoneyTransferJpaRepository;
 import nc.sgcb.labs.commons.domain.Amount;
 import nc.sgcb.labs.commons.domain.Iban;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Objects;
 
 @Tag(name = "Accounts")
 @RestController
@@ -58,8 +64,8 @@ public class AccountController {
   public PagedModel<MoneyTransferResponse> listTransfers(
       @Parameter(schema = @Schema(type = "string"))
       @PathVariable(name = ACCOUNT_PLACEHOLDER) Account account,
-      @Valid MoneyTransferFilterRequest dto,
-      Pageable pageable) {
+      @Valid @ParameterObject MoneyTransferFilterRequest dto,
+      @ParameterObject Pageable pageable) {
     var criteria = transferMapper.map(dto);
     var transferPage =
         transferRepo.findAll(MoneyTransferJpaRepository.searchSpec(criteria), pageable);
